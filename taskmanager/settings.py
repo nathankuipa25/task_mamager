2298fb99fa7dc612b5045014d3647a99d529f672
Nathan Kuipa <nathankuipa44@gmail.com>
2025-08-28 03:19:19 +0200

    Update settings.py

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*7d8y7(scjmx86r$c0+mdo4w+wp7tyuuk7^pgad5f1l4zp7#o_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["taskaapi.onrender.com"]
 #CSRF_TRUSTED_ORIGINS = [
